ede3b9c624213560db44b3b2579a8ceb59dd53fc
Mazas <info.otero.gabriel@gmail.com>
2022-04-25 14:03:54 +0200

    25-04-2022-14-03

--- 03-AdvancedSql/08-AdvancedSelectionClauses/08-OteroLopezGabriel.sql
+++ 03-AdvancedSql/08-AdvancedSelectionClauses/08-OteroLopezGabriel.sql
@@ -12,24 +12,24 @@
 12	SELECT oficio, iddepart, COUNT(codemp) AS numero, SUM(salario) AS suma FROM empleados GROUP BY oficio, iddepart;
 13	SELECT iddepart, oficio, COUNT(codemp) AS numero FROM empleados GROUP BY oficio, iddepart HAVING numero>2;
 14	SELECT iddepart, COUNT(DISTINCT oficio) AS numero FROM empleados GROUP BY iddepart;
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
+15	SELECT codemp,salario from empleados WHERE iddepart=(select iddepart from depart where nombre='ventas');
+16	SELECT codemp,apellido,salario from empleados WHERE salario>(select salario FROM empleados where codemp=6);
+17	SELECT apellido,oficio from empleados WHERE oficio=(select oficio FROM empleados where apellido='Jimenez') AND apellido!='Jimenez';
+18	SELECT apellido,salario from empleados WHERE oficio=(select oficio FROM empleados where apellido='Gil') AND salario=(select salario FROM empleados where apellido='Gil') AND apellido!='Gil';
+19	SELECT apellido,oficio,salario,fecha_alt from empleados WHERE oficio=(SELECT oficio FROM empleados WHERE apellido='Jimenez') OR salario>= (SELECT salario FROM empleados WHERE apellido='Fernandez');
+20	SELECT iddepart, COUNT(apellido) from empleados WHERE oficio='empleado' GROUP BY iddepart;
+21	SELECT * FROM empleados WHERE salario>(SELECT AVG(salario) FROM empleados);
+22	SELECT apellido,oficio,salario FROM empleados WHERE iddepart=(SELECT iddepart FROM empleados WHERE apellido='Fernandez') AND salario =(SELECT salario FROM empleados WHERE apellido='Fernandez') AND apellido!='Fernandez';
+23	SELECT iddepart,AVG(salario) AS media FROM empleados GROUP BY iddepart HAVING media>=(SELECT AVG(salario) FROM	empleados); 
+24	SELECT iddepart,COUNT(apellido) AS numero FROM empleados WHERE iddepart=(SELECT iddepart FROM depart WHERE nombre='ventas') AND oficio='vendedor';
+25	SELECT apellido,comision FROM empleados WHERE comision>(SELECT comision FROM empleados WHERE codemp=3);
+26	SELECT COUNT(apellido) FROM empleados WHERE oficio='director'AND iddepart=(SELECT iddepart FROM depart WHERE nombre='ventas');
+27	SELECT iddepart,MIN(salario) AS salario FROM empleados GROUP BY iddepart HAVING iddepart!=(SELECT iddepart FROM depart WHERE loc='Madrid');
+28	SELECT apellido,iddepart FROM empleados where iddepart=(SELECT iddepart FROM depart WHERE loc='Madrid') OR iddepart=(SELECT iddepart FROM depart WHERE loc='Barcelona') ORDER BY iddepart;
+29	SELECT iddepart,nombre FROM depart WHERE iddepart NOT IN (SELECT iddepart FROM empleados);
+30	SELECT apellido,oficio,comision FROM empleados WHERE iddepart NOT IN (SELECT iddepart FROM depart WHERE loc='Sevilla' OR loc='Madrid') AND comision IS null;
+31	SELECT apellido,oficio FROM empleados WHERE iddepart=20 AND oficio IN (SELECT oficio FROM empleados WHERE iddepart=(SELECT iddepart FROM depart WHERE nombre='ventas'));
+32	SELECT nombre FROM asignaturas WHERE cod IN (SELECT asignatura FROM notas WHERE nota>4);
 33
 34
 35
